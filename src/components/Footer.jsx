d186f8b6e00fc71e9b818c1ad399bf6453b5f5c2
Basant Elsaey <basantelsaey728@gmail.com>
2025-06-10 05:56:31 +0300

    Footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer footer-center p-10 kainate">
+      <div className="container mx-auto">
+        <p className="text-sm">© 2025 Blog System by Zag. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
